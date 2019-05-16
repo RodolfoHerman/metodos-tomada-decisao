a46ba50b27aa79afe7077655eed18a8ad2e6e639
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2019-05-16 17:51:12 -0300

    Realizando o bind dos botões

--- src/main/java/br/com/rodolfo/decisao/controllers/TelaController.java
+++ src/main/java/br/com/rodolfo/decisao/controllers/TelaController.java
@@ -3,9 +3,11 @@ package br.com.rodolfo.decisao.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.ToggleGroup;
 
@@ -28,7 +30,20 @@ public class TelaController implements Initializable {
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        
+        metodos.getToggles().forEach(node -> {
 
+            RadioButton radio = (RadioButton) node;
+            radio.setDisable(true);
+        });
+
+        problemas.getToggles().forEach(node -> {
+
+            RadioButton radio = (RadioButton) node;
+            radio.setDisable(true);
+        });
+
+        btnAnalisar.setDisable(true);
     }
 
     @FXML
@@ -46,6 +61,8 @@ public class TelaController implements Initializable {
     @FXML
     public void fecharPrograma() {
         
+        Platform.exit();
+        System.exit(0);
     }
 
 }
